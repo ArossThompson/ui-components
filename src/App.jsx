a4227734ea11422dbe3440475baafeeb6aa3ae7c
ross thompson <rossathompson94@gmail.com>
2020-11-12 12:48:39 +0000

    removed some CRA template from public and added bootstrap to HTML linked stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import './app.scss';
+
+// Component imports
+import Nav from './components/Nav';
+
 
 const App = () => {
   return (
     <div>
-      
+      <Nav />
     </div>
   )
 }
